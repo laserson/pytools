2404f1b95105bd990b41bdc0960a89091ebea544
Uri Laserson <laserson@mit.edu>
2010-04-15 17:34:43 -0400

    minor changes

--- statstools.py
+++ statstools.py
@@ -1,4 +1,10 @@
 import numpy as np
 
+randint = np.random.randint
+
 def choose(l):
-    return l[np.random.randint(len(l))]
+    return l[randint(len(l))]
+
+def random_read(seq,read_len):
+    position = randint(0,len(seq)-read_len+1)
+    return (position,seq[position:position+read_len])

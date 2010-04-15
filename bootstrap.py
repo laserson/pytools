8430d364aa3e932079159a86c2dbd72e3a02bac7
Uri Laserson <laserson@mit.edu>
2010-04-14 22:53:27 -0400

    Cleaned up implementation of bootstrap (using map)

--- bootstrap.py
+++ bootstrap.py
@@ -1,16 +1,20 @@
-# bootstrap.py
+# 
+#  bootstrap.py
+#  pytools element
+#  
+#  Created by Uri Laserson on 2008-12-11.
+#  Copyright 2010 Uri Laserson. All rights reserved.
+# 
 
-import numpy
-
-# INITIALIZATIONS
+import numpy as np
 
 numpy.random.seed()
 # numpy.random.seed(1)
 
-permutation = numpy.random.permutation
-randint     = numpy.random.randint
+permutation = np.random.permutation
+randint     = np.random.randint
 
-def permsamp(x, nperm, theta, *args):
+def permsamp(x, nperm, theta):
     '''sample nperm times from the permutation distribution of the data x (numpy)
        theta is the function that takes the data and computes the statistic
        it must know how the data is "encoded" in x
@@ -18,20 +22,22 @@ def permsamp(x, nperm, theta, *args):
        returns a vector of nperm th_star values
     '''
     N = len(x)
-    th_star = numpy.zeros(nperm)
+    th_star = np.asarray( map(theta,x[permutation(N)]) )
     
-    for i in xrange(nperm):
-        th_star[i] = theta( x[ permutation(N) ], *args )
+    # th_star = numpy.zeros(nperm)
+    # for i in xrange(nperm):
+    #     th_star[i] = theta( x[ permutation(N) ], *args )
     
     return th_star
 
-def bootstrap(x, nboot, theta, *args):
+def bootstrap(x, nboot, theta):
     '''return n bootstrap replications of theta from x'''
     
     N = len(x)
-    th_star = numpy.zeros(nboot)
+    th_star = np.asarray( map(theta,x[randint(0,N,N)]) )
     
-    for i in xrange(nboot):
-        th_star[i] = theta( x[ randint(0,N,N) ], *args )    # bootstrap repl from x
+    # th_star = numpy.zeros(nboot)
+    # for i in xrange(nboot):
+    #     th_star[i] = theta( x[ randint(0,N,N) ], *args )    # bootstrap repl from x
     
     return th_star

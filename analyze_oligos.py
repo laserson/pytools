8d511379237c8e359d4bf2884553135c4a35eefc
Uri Laserson <laserson@mit.edu>
2010-05-07 16:39:01 -0400

    Script to analyze primers

---
+++ analyze_oligos.py
@@ -0,0 +1,58 @@
+import sys
+
+from Bio import SeqIO
+import numpy as np
+
+import seqtools
+import oligoTm
+import unafold
+import blast
+
+# ==================
+# = Output primers =
+# ==================
+
+def output_primers(primers,names):
+    datum = ('name','sequence','len','Tm',r'%GC','ss-dG','BLAST')
+    header = "\n%-25s %-30s %-4s %-5s %-4s %-7s %-5s\n" % datum
+    sys.stdout.write(header)
+    
+    lens = map(len,primers)
+    Tms = map(oligoTm.oligo_Tm,primers)
+    gcs = map(lambda p: seqtools.gc_content(p)*100,primers)
+    dGs = map(lambda p: unafold.hybrid_ss_min(p,NA='DNA',sodium=0.05),primers)
+    trunc_primers = [p[-min(18,min(lens)):] for p in primers]
+    seqrecords = map(lambda t: seqtools.make_SeqRecord(*t),zip(names,trunc_primers))
+    blast_hits = blast.number_genome_qblast_hits(seqrecords)
+    
+    for datum in zip(names,primers,lens,Tms,gcs,dGs,blast_hits):
+        primer_string = "%-25s %-30s %-4i %-5.1f %-4.0f %-7.1f %-5i\n" % datum
+        sys.stdout.write(primer_string)
+    
+    summary_data = lambda d: (np.mean(d),np.std(d),np.min(d),np.max(d))
+    
+    sys.stdout.write('\nsummary:\n')
+    sys.stdout.write('num primers: %i\n' % len(primers))
+    sys.stdout.write('len    mean: %5.1f    std: %5.1f    min: %5.1f    max %5.1f\n' % summary_data(lens))
+    sys.stdout.write('Tm     mean: %5.1f    std: %5.1f    min: %5.1f    max %5.1f\n' % summary_data(Tms))
+    sys.stdout.write('%%GC    mean: %5.1f    std: %5.1f    min: %5.1f    max %5.1f\n' % summary_data(gcs))
+    sys.stdout.write('dGs    mean: %5.1f    std: %5.1f    min: %5.1f    max %5.1f\n' % summary_data(dGs))
+    sys.stdout.write('BLAST  mean: %5.1f    std: %5.1f    min: %5.1f    max %5.1f    total: %5.1f\n' % (summary_data(blast_hits)+(np.sum(blast_hits),)))
+
+if __name__ == '__main__':
+    
+    if len(sys.argv) == 3:
+        inhandle = open(sys.argv[1],'r')
+        outhandle = open(sys.argv[2],'w')
+    elif len(sys.argv) == 2:
+        inhandle = open(sys.argv[1],'r')
+        outhandle = sys.stdout
+    elif len(sys.argv) == 1:
+        inhandle = sys.stdin
+        outhandle = sys.stdout
+    
+    seqrecords = list(SeqIO.parse(inhandle,'fasta'))
+    names = [rec.id for rec in seqrecords]
+    primers = [seqtools.get_string(rec) for rec in seqrecords]
+    
+    output_primers(primers,names)

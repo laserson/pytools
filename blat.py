5b0b37c6093f88bbd09177cda20755fad2c5d3a0
Uri Laserson <laserson@mit.edu>
2010-05-11 14:46:24 -0400

    BLAT tools

---
+++ blat.py
@@ -0,0 +1,34 @@
+# BLAT tools
+# based on Sri's code
+
+import subprocess
+import os
+
+hg_idx = ''
+
+, shell=True, stdin=None, stdout=PIPE, stderr=STDOUT, close_fds=True
+
+
+def start_gfServer(file2idx,tileSize=7,stepSize=2,minMatch=1,maxGap=4):
+    params = (tileSize,stepSize,minMatch,maxGap,file2idx)
+    cmd = "gfServer -tileSize=%i -stepSize=%i -minMatch=%i -maxGap=%i start localhost 17779 %s" % params
+    p = subprocess.Popen(cmd,shell=True)
+    return p
+
+def stop_gfServer(p):
+    cmd = "gfServer stop localhost 17779"
+    q = subprocess.Popen(cmd,shell=True)
+    return q
+
+def poll_sequence(seq,file2idx,minScore=10,minIdentity=70):
+    nibdir = os.path.dirname(file2idx)
+    params = (minScore,minIdentity,nibdir)
+    cmd = "gfClient -minScore=%i -minIdentity=%i -nohead localhost 17779 %s /dev/stdin /dev/stdout" % params
+    p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+    p.communicate(">query\n%s" % seq)
+    
+    num = 0
+    for line in p.stdout:
+        num += 1
+    
+    return num

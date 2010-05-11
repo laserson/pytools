5e6260565db510dcc656c645c4d1f16282370bb2
Uri Laserson <laserson@mit.edu>
2010-05-11 15:25:43 -0400

    Debugging

--- blat.py
+++ blat.py
@@ -21,19 +21,9 @@ def poll_sequence(seq,file2idx,minScore=10,minIdentity=70):
     nibdir = os.path.dirname(file2idx)
     params = (minScore,minIdentity,nibdir)
     cmd = "gfClient -minScore=%i -minIdentity=%i -nohead localhost 17779 %s /dev/stdin /dev/stdout" % params
-    import sys
-    print "About to initiate command"
-    sys.stdout.flush()
     p = subprocess.Popen(cmd,shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
-    print "Initiated command.  Will now communicate with process."
-    sys.stdout.flush()
-    import cStringIO
     p.stdin.write(">query\n%s\n" % seq)
     p.stdin.close()
-    # p.stdin.write(cStringIO.StringIO(">query\n%s\n" % seq).read())
-    # p.communicate(">query\n%s" % seq)
-    print "Finished communicating.  Waiting now..."
-    sys.stdout.flush()
     
     print p.stdout.read()
     
@@ -43,7 +33,4 @@ def poll_sequence(seq,file2idx,minScore=10,minIdentity=70):
     #     sys.stdout.flush()
     #     num += 1
     
-    print "Finished with output.  Returning"
-    sys.stdout.flush()
-    
-    return num
+    # return num

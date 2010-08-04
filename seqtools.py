cac19b73f66ca3b7caeb61b04eb481da4263e7e8
Uri Laserson <laserson@mit.edu>
2010-08-04 18:37:53 -0400

    Added cleanup_id fn for safe filenames

--- seqtools.py
+++ seqtools.py
@@ -10,13 +10,16 @@ random.seed()
 def substitute(seq,pos,sub):
     return seq[:pos] + sub + seq[pos+1:]
 
+
 complement_table = string.maketrans('ACGTRYSWKMBDHVN','TGCAYRSWMKVHDBN')
 
 def reverse(seq):
     return seq[::-1]
 
+
 def complement(seq):
     return seq.upper().translate(complement_table)
+
 
 def reverse_complement(seq):
     """Compute reverse complement of sequence.
@@ -28,14 +31,21 @@ def reverse_complement(seq):
     # return seq.upper().translate(complement_table)[::-1]
 
 
-
 def gc_content(seq):
     gc = seq.lower().count('g') + seq.lower().count('c')
     return float(gc) / len(seq)
 
+
 def random_dna_seq(n):
     choice = random.choice
     return reduce(lambda cumul,garbage:cumul+choice('ACGT'),xrange(n),'')
+
+
+# for generating 'safe' filenames from identifiers
+cleanup_table = string.maketrans('/*|','___')
+def cleanup_id(identifier):
+    return identifier.translate(cleanup_table)
+
 
 # ==========================
 # = Manual FASTA iteration =
@@ -70,6 +80,7 @@ def FastaIterator(handle,title2ids=lambda s: s):
         if not line : return #StopIteration
     assert False, "Should not reach this line"
 
+
 # ============================
 # = biopython-specific tools =
 # ============================
@@ -80,6 +91,7 @@ from Bio.SeqRecord import SeqRecord
 def make_SeqRecord(name,seq):
     return SeqRecord(Seq(seq),id=name,name=name,description=name)
 
+
 def get_string(seqobj):
     if isinstance(seqobj,SeqRecord):
         seq = seqobj.seq.tostring().upper()
@@ -89,6 +101,7 @@ def get_string(seqobj):
         seq = seqobj.upper()
     return seq
 
+
 def get_features(feature_list,feature_type):
     target_features = []
     for feature in feature_list:
@@ -96,12 +109,14 @@ def get_features(feature_list,feature_type):
             target_features.append(feature)
     return target_features
 
+
 def advance_to_features(feature_iter,feature_types):
     # note, here feature_types is a list of possible stopping points
     for feature in feature_iter:
         if feature.type in feature_types:
             return feature
     raise ValueError, "didn't find %s in record" % feature_types
+
 
 def advance_to_feature(feature_iter,feature_type):
     return advance_to_features(feature_iter,[feature_type])

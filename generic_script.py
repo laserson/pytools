b552cbf1fe1a621f018717d623fd2abcf3d594b4
Uri Laserson <laserson@mit.edu>
2010-08-04 18:37:29 -0400

    Corrected a mistake and added another option in genericscript

--- generic_script.py
+++ generic_script.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 if __name__ == '__main__':
+    import sys
     import optparse
     
     option_parser = optparse.OptionParser()
@@ -14,5 +15,22 @@ if __name__ == '__main__':
         inhandle = open(args[0],'r')
         outhandle = sys.stdout
     elif len(args) == 0:
+        inhandle = sys.stdin
+        outhandle = sys.stdout
+
+
+
+# OR
+
+if __name__ == '__main__':
+    import sys
+    
+    if len(sys.argv) == 3:
+        inhandle = open(sys.argv[1],'r')
+        outhandle = open(sys.argv[2],'w')
+    elif len(sys.argv) == 2:
+        inhandle = open(sys.argv[1],'r')
+        outhandle = sys.stdout
+    elif len(sys.argv) == 1:
         inhandle = sys.stdin
         outhandle = sys.stdout

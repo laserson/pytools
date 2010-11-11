98d810ebd89161d217f1ddde44399ed10ae9f9ca
Uri Laserson <laserson@mit.edu>
2010-11-11 14:45:31 -0500

    Starting to write function for bidirec sesq

--- sanger.py
+++ sanger.py
@@ -63,3 +63,39 @@ def trimright(right,read):
     
     # return trimmed sequence
     return read[:aln['target_aln_begin']]
+
+# ===============
+# = UNFINISHED: =
+# ===============
+
+def bidirectional_alignment(forward,reverse):
+    """Align forward and reverse sequence of bidirectional Sanger reads.
+    
+    forward and reverse sequences must already be in the same 'sense' (i.e.,
+    reverse should be revcomped if necessary so that both strands in alignment
+    are '+').
+    
+    Forces alignment of both ends (right end of forward, and left end of
+    reverse).
+    
+    ...FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+                               RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR...
+    
+    Uses exonerate.
+    """
+    # perform alignment
+    cmd = exonerate.ExonerateCommand('findend','parsable','bestonly')
+    rawaln = exonerate.run_exonerate2(cmd,forward,reverse)
+    aln = exonerate.parse_aln(rawaln)
+    
+    # check that both strands are + orientation
+    if aln['query_strand'] != '+':
+        raise ValueError, "query strand has been reversed"
+    if aln['target_strand'] != '+':
+        raise ValueError, "target strand has been reversed"
+    
+    # check that right end of forward and left end of reverse are placed
+    if aln['query_len'] != aln['query_aln_end']:
+        raise ValueError, "failed to align right-end of forward sequence"
+    if aln['query_aln_begin'] != 0:
+        raise ValueError, "failed to align left-end of right sequence"

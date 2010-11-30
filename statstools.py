c86381d2ad46c4b4f05fbc71079405065f34821d
Uri Laserson <laserson@mit.edu>
2010-11-29 20:18:57 -0500

    Added sample_with_replacement fn

--- statstools.py
+++ statstools.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 
 randint = np.random.randint
@@ -8,3 +10,13 @@ def choose(l):
 def random_read(seq,read_len):
     position = randint(0,len(seq)-read_len+1)
     return (position,seq[position:position+read_len])
+
+def sample_with_replacement(population,len,choose=random.choice):
+    """Sample from a population with replacement
+    
+    Taken from Python Cookbook, 2nd ed, recipe 18.3
+    """
+    s = []
+    for i in xrange(len):
+        s.append(choose(population))
+    return s

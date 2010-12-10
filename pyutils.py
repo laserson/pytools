080f4364046841e0312939c177b506dc13f2bb9a
Uri Laserson <uri.laserson@gmail.com>
2010-12-09 23:25:40 -0500

    Added nesteddict class

--- pyutils.py
+++ pyutils.py
@@ -1,11 +1,26 @@
-class ModuleWrapper(object):
-    """Wrap a module to allow user-defined __getattr__
+import collections
+
+class nesteddict(collections.defaultdict):
+    def __init__(self):
+        defaultdict.__init__(self, nesteddict)
     
-    see http://stackoverflow.com/questions/2447353/getattr-on-a-module
-    """
-    def __init__(self, module, usergetattr):
-        self.module = module
-        self.usergetattr = usergetattr
-    
-    def __getattr__(self, name):
-        return self.usergetattr(self,name)
+    def walk(self):
+        for (key,value) in self.iteritems():
+            if isinstance(value, nesteddict):
+                for tup in value.walk():
+                    yield (key,) + tup
+            else:
+                yield (key,value)
+
+
+# class ModuleWrapper(object):
+#     """Wrap a module to allow user-defined __getattr__
+#     
+#     see http://stackoverflow.com/questions/2447353/getattr-on-a-module
+#     """
+#     def __init__(self, module, usergetattr):
+#         self.module = module
+#         self.usergetattr = usergetattr
+#     
+#     def __getattr__(self, name):
+#         return self.usergetattr(self,name)

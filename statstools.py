0c07e3495897427de171b9ae49d614af81607077
Uri Laserson <uri.laserson@gmail.com>
2010-12-15 11:04:08 -0500

    removed fn duplicated from random module

--- statstools.py
+++ statstools.py
@@ -2,11 +2,6 @@ import random
 
 import numpy as np
 
-randint = np.random.randint
-
-def choose(l):
-    return l[randint(len(l))]
-
 def random_read(seq,read_len):
     position = randint(0,len(seq)-read_len+1)
     return (position,seq[position:position+read_len])

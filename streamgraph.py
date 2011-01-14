c2e29fc39cbb3ea817c0e81e0b32e44c422eb74a
Uri Laserson <uri.laserson@gmail.com>
2011-01-14 18:35:42 -0500

    Finished initial MPL streamgraph fns

--- streamgraph.py
+++ streamgraph.py
@@ -8,95 +8,87 @@ import matplotlib as mpl
 # mpl.use('Agg')
 import matplotlib.pyplot as plt
 
+# baseline functions
+def symmetric(streams):
+    """Symmetric baseline ('silhouette')"""
+    g0 = -0.5 * np.sum(np.asarray(streams),axis=0)
+    return g0
 
-def symmetric(sorted_streams, stream_bounds):
-    """Symmetric baseline"""
-    lb, ub = np.min(stream_bounds[:,0,:],axis=0), np.max(stream_bounds[:,1,:],axis=0)
-    return .5*(lb+ub)
-
-def pos_only(sorted_streams, stream_bounds):
-    """Lumps will only be positive"""
-    lb, ub = np.min(stream_bounds[:,0,:],axis=0), np.max(stream_bounds[:,1,:],axis=0)
-    return lb
-def zero(sorted_streams, stream_bounds):
+def zero(streams):
     """Zero baseline"""
-    return np.zeros(stream_bounds.shape[2])
-def min_weighted_wiggles(sorted_streams, stream_bounds):
-    """Baseline recommended by Byron and Wattenberg"""
-    lb, ub = np.min(stream_bounds[:,0,:],axis=0), np.max(stream_bounds[:,1,:],axis=0)
-    weight = ub-lb
-    
-    sorted_streams = np.abs(sorted_streams)
-    for i in xrange(len(sorted_streams)):
-        sorted_streams[i,:] *= (-1)**i
-    cusum_f = np.vstack((np.zeros(sorted_streams.shape[1]),
-                        np.cumsum(sorted_streams[:-1,:], axis=0)))
-    f_prime = np.diff(sorted_streams, axis=1)
-    cusum_f_prime = np.diff(cusum_f, axis=1)
-    g_prime = np.hstack(([0],-np.sum((f_prime*.5  + cusum_f_prime)*sorted_streams[:,1:],axis=0) / weight[1:]))
-    g_prime[np.where(weight==0)] = 0
-    g = np.cumsum(g_prime)
-    
-    return g
-    
+    return np.zeros(np.asarray(streams).shape[1])
 
-def streamgraph(streams, cmap=pl.cm.bone, color_seq='linear', baseline_fn=min_weighted_wiggles):
-    """
-    Produces stacked graphs using matplotlib.
+def weighted_wiggle(streams):
+    """Weighted-wiggle minimization
     
-    Reference: 'Stacked graphs- geometry & aesthetics' by Byron and Wattenberg
-    http://www.leebyron.com/else/streamgraph/download.php?file=stackedgraphs_byron_wattenberg.pdf
-    
-    Parameters:
-      - streams: A list of time-series of positive values. Each element must be of the same length.
-      - cmap: A matplotlib color map. Defaults to 'bone'.
-      - colo_seq: 'linear' or 'random'.
-      - baseline_fn: Current options are symmetric, pos_only, zero and min_weighted_wiggles.
+    NOTE: streams should already be ordered as desired
     """
-    # Sort by onset times
-    onset_times = [np.where(np.abs(stream)>0)[0][0] for stream in streams]
-    order = np.argsort(onset_times)
     streams = np.asarray(streams)
-    sorted_streams = streams[order]
     
-    t = np.arange(streams.shape[1])
-    
-    # Establish bounds
-    stream_bounds = [np.vstack((np.zeros(streams.shape[1]), sorted_streams[0])),
-                    np.vstack((-sorted_streams[1], (np.zeros(streams.shape[1]))))]
-    side = -1
-    for stream in sorted_streams[2:]:
-        side *= -1
-        if side==1:
-            stream_bounds.append(np.vstack((stream_bounds[-2][1], stream_bounds[-2][1]+stream)))
+    # add a column of zeros on the left side of streams
+    f = np.hstack( (np.zeros((streams.shape[0],1)),streams) )
+    df = np.diff(f)
+    cum_sum_df = np.vstack( (np.zeros((1,df.shape[1])),np.cumsum(df,axis=0)) )[:-1,:]
+    dg0 = (-1./np.sum(streams,axis=0)) * np.sum((0.5 * df + cum_sum_df) * streams,axis=0)
+    g0 = np.cumsum(dg0)
+    return g0
+
+# ordering functions
+def onset(streams):
+    """Returns permutation indices (like argsort) for onset ordering."""
+    streams = np.asarray(streams)
+    nonzero_idxs = [np.arange(streams.shape[1])[idxs] for idxs in (streams > 0)]
+    onset_idxs = [np.min(nzi) if len(nzi) > 0 else streams.shape[1] for nzi in nonzero_idxs]
+    return np.argsort(onset_idxs)
+
+def inside_out(streams):
+    """Returns permutation indices (like argsort) for inside-out ordering."""
+    upper = []
+    lower = []
+    weight_up = 0
+    weight_lo = 0
+    for (i,stream) in enumerate(streams):
+        if weight_up >= weight_lo:
+            upper.append(i)
+            weight_up += np.sum(stream)
         else:
-            stream_bounds.append(np.vstack((stream_bounds[-2][0]-stream, stream_bounds[-2][0])))
-            
-    stream_bounds = np.array(stream_bounds)
+            lower.append(i)
+            weight_lo += np.sum(stream)
     
-    # Compute baseline
-    baseline = baseline_fn(sorted_streams, stream_bounds)
+    return upper + lower
+
+def streamgraph(ax,streams, x=None, colors=None, baseline=weighted_wiggle):
+    streams = np.asarray(streams)
     
-    # Choose colors
-    t_poly = np.hstack((t,t[::-1]))
-    if color_seq=='linear':
-        colors = np.linspace(0,1,streams.shape[1])
-    elif color_seq=='random':
-        colors = np.random.random(size=streams.shape[1])
-    else:
-        raise ValueError, 'Color sequence %s unrecognized'%color_seq
+    g0 = baseline(streams)
     
-    # Plot    
-    pl.axis('off')        
-    for i in xrange(len(stream_bounds)):
-        bound = stream_bounds[i]
-        color = cmap(colors[i])
-        pl.fill(t_poly, np.hstack((bound[0]-baseline,(bound[1]-baseline)[::-1])), facecolor=color, linewidth=0.,edgecolor='none')
-        
+    if x == None:
+        x = range(streams.shape[1])
+    
+    if colors == None:
+        colors = map(mpl.cm.bone,np.random.uniform(size=streams.shape[0]))
+    
+    layers = []
+    g_lo = g0
+    for stream in streams:
+        g_hi = g_lo + stream
+        verts_lo = zip(x,g_lo)
+        verts_hi = zip(x[::-1],g_hi[::-1])
+        layer = verts_lo + verts_hi
+        layers.append(layer)
+        g_lo = g_hi
+    
+    polys = mpl.collections.PolyCollection(layers,facecolors=colors,linewidths=0)
+    ax.add_collection(polys)
+    
+    return ax
+    
         
 # Demo
 if __name__ == '__main__':
-    pl.clf()
+    np.random.seed(1)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
     N_dsets = 50
     T = 100
     amp = 1
@@ -112,5 +104,9 @@ if __name__ == '__main__':
         this_dset[max(t_onset,0):]=np.exp(-.15*np.random.gamma(10,.1)*remaining_t)\
                             * remaining_t * np.random.gamma(6,.2)# * np.cos(-fade*remaining_t*np.random.gamma(10,.1))**2
         dsets.append(this_dset)
-    stacked_graph(dsets, baseline_fn = min_weighted_wiggles, color_seq='random')
+    import pdb
+    pdb.set_trace()
+    streamgraph(ax,dsets,baseline=symmetric)
+    ax.autoscale_view()
+    plt.draw()
 ## end of http://code.activestate.com/recipes/576633/ }}}

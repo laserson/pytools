ad8ca77219c2721c6e7c317891b8c8a8310ee019
Uri Laserson <uri.laserson@gmail.com>
2011-05-05 11:30:56 -0400

    Added unafold fn for lists

--- unafold.py
+++ unafold.py
@@ -1,3 +1,6 @@
+import os
+import glob
+import tempfile
 import subprocess
 
 def hybrid_ss_min(seq,NA='RNA',tmin=37,tinc=1,tmax=37,sodium=1,magnesium=0):
@@ -13,3 +16,32 @@ def hybrid_min(seq1,seq2,NA='RNA',tmin=37,tinc=1,tmax=37,sodium=1,magnesium=0):
     p.wait()
     dG = float(p.stdout.read().split()[0])
     return dG
+
+def hybrid_min_list(seqlist1,seqlist2,NA='RNA',tmin=37,tinc=1,tmax=37,sodium=1,magnesium=0):
+    # set up temporary files
+    temp_out_prefix = 'temporary_hybrid_6157'
+    seqfile1 = tempfile.NamedTemporaryFile(mode='w',dir='.',prefix='hybrid_min_temp',suffix='.fasta')
+    seqfile2 = tempfile.NamedTemporaryFile(mode='w',dir='.',prefix='hybrid_min_temp',suffix='.fasta')
+    for (i,seq) in enumerate(seqlist1): print >>seqfile1, ">1_%i\n%s" % (i,seq)
+    for (i,seq) in enumerate(seqlist2): print >>seqfile2, ">2_%i\n%s" % (i,seq)
+    seqfile1.file.flush()
+    seqfile2.file.flush()
+    
+    # set up and execute command
+    cmd = 'hybrid-min --NA=%s --tmin=%f --tinc=%f --tmax=%f --sodium=%f --magnesium=%f --output=%s %s %s' % (NA,tmin,tinc,tmax,sodium,magnesium,temp_out_prefix,seqfile1.name,seqfile2.name)
+    p = subprocess.Popen(cmd,shell=True)
+    p.wait()
+    
+    # read results
+    ip = open(temp_out_prefix+'.dG','r')
+    dGs = []
+    for line in ip:
+        if line.startswith('#'): continue
+        dGs.append(float(line.split()[1]))
+    
+    # clean up output
+    seqfile1.close()
+    seqfile2.close()
+    for filename in glob.glob(temp_out_prefix+'*'): os.remove(filename)
+    
+    return dGs

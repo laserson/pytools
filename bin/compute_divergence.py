88c29e9848659c069049a3ed45faa9796c83f06b
Uri Laserson <uri.laserson@gmail.com>
2011-12-04 19:16:01 -0500

    added option to specify executable

--- bin/compute_divergence.py
+++ bin/compute_divergence.py
@@ -7,9 +7,10 @@ argparser = argparse.ArgumentParser(description=None)
 argparser.add_argument('-q','--query',required=True)
 argparser.add_argument('-t','--target',required=True)
 argparser.add_argument('-o','--output',required=True)
+argparser.add_argument('-u','--usearch',default='usearch')
 args = argparser.parse_args()
 
-usearch_cmd = "usearch --query %s --db %s --nofastalign --maxaccepts 0 --maxrejects 0 --global --id 0 --userout %s --userfields query+target+id0+id1+id2+id3+id4+gaps+intgaps+qloz+qhiz+tloz+thiz+ql+tl+cols+intcols"
+usearch_cmd = "%s --query %s --db %s --nofastalign --maxaccepts 0 --maxrejects 0 --global --id 0 --userout %s --userfields query+target+id0+id1+id2+id3+id4+gaps+intgaps+qloz+qhiz+tloz+thiz+ql+tl+cols+intcols"
 
-p = subprocess.Popen(usearch_cmd % (args.query,args.target,args.output),shell=True)
+p = subprocess.Popen(usearch_cmd % (args.usearch,args.query,args.target,args.output),shell=True)
 p.wait()

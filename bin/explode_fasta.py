e686939e3af6272a9de30b75b61341f049023979
Uri Laserson <uri.laserson@gmail.com>
2012-01-13 16:29:36 -0500

    added explode_fasta

---
+++ bin/explode_fasta.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import argparse
+
+import vdj
+from pyutils import cleanup_id
+
+argparser = argparse.ArgumentParser(description=None)
+argparser.add_argument('input_file',default=sys.stdin)
+argparser.add_argument('output_dir',default=os.getcwd())
+args = argparser.parse_args()
+
+for chain in vdj.parse_imgt(args.input_file):
+    output_file = os.path.join(args.output_dir,'%s.imgt' % cleanup_id(chain.id))
+    with open(output_file,'w') as op:
+        print >>op, chain

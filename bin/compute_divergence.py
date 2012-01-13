e68c9c4415f9aa01d5b683cec54a24732637dd90
Uri Laserson <uri.laserson@gmail.com>
2012-01-13 17:35:54 -0500

    modified options to usearch

--- bin/compute_divergence.py
+++ bin/compute_divergence.py
@@ -10,7 +10,7 @@ argparser.add_argument('-o','--output',required=True)
 argparser.add_argument('-u','--usearch',default='usearch')
 args = argparser.parse_args()
 
-usearch_cmd = "%s --query %s --db %s --nofastalign --maxaccepts 0 --maxrejects 0 --global --id 0 --userout %s --userfields query+target+id0+id1+id2+id3+id4+gaps+intgaps+qloz+qhiz+tloz+thiz+ql+tl+cols+intcols"
+usearch_cmd = "%s --query %s --db %s --nofastalign --nousort --minlen 0 --maxaccepts 0 --maxrejects 0 --global --id 0 --userout %s --userfields query+target+id0+id1+id2+id3+id4+gaps+intgaps+qloz+qhiz+tloz+thiz+ql+tl+cols+intcols"
 
 p = subprocess.Popen(usearch_cmd % (args.usearch,args.query,args.target,args.output),shell=True)
 p.wait()

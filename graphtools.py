bb9a7f55a9705fdb81212cb3d2f86ccbb892345e
Uri Laserson <uri.laserson@gmail.com>
2012-01-30 00:29:56 -0500

    Added module for graph drawing etc

--- graphtools.py
+++ graphtools.py
@@ -1,5 +1,7 @@
 import pygraphviz as pgv
 
+import scale
+
 def load_immunitree_nodes(infile):
     G = pgv.AGraph(strict=True,directed=True)
     with open(infile,'r') as ip:
@@ -12,9 +14,28 @@ def load_immunitree_nodes(infile):
             size = int(data[2])
             muts = len(data[-1].split('-'))
             
-            G.add_node(node,size=size)
+            G.add_node(node,xlabel="[%s] %i" % (node,size),size=size)
             if parent != '0':
-                G.add_edge(parent,node,mut=muts)
+                G.add_edge(parent,node,label=muts)
     
     return G
 
+def format_immunitree_graph(G):
+    min_size = max(min([int(node.attr['size']) for node in G.nodes_iter()]),1)
+    max_size = max([int(node.attr['size']) for node in G.nodes_iter()])
+    min_area = 0.3
+    max_area = 1.3
+    area_scale = scale.root(min_size,max_size).range(min_area,max_area).power(2)
+    for node in G.nodes_iter():
+        node.attr['fixedsize'] = True
+        if int(node.attr['size']) == 0:
+            node.attr['shape'] = 'point'
+        else:
+            node.attr['shape'] = 'circle'
+            node.attr['height'] = area_scale(int(node.attr['size']))
+    
+    for edge in G.edges_iter():
+        pass
+    
+    G.graph_attr['forcelabels'] = True
+    G.layout(prog='dot')

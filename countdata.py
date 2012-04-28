ecf9ed683766a8b5017552a73819662292e151cb
Uri Laserson <uri.laserson@gmail.com>
2012-04-27 20:07:05 -0700

    bugfix

--- countdata.py
+++ countdata.py
@@ -184,10 +184,12 @@ def pval_KalZtest_vec(n1,N1,n2,N2):
     p1 = n1/N1
     p2 = n2/N2
     
+    p0[(n1 == 0) & (n2 == 0)] = 0.5
+    
     Z = (p1-p2) / np.sqrt( p0 * (1-p0) * ((1/N1) + (1/N2)) )
     
     pval = 2 * sp.stats.norm.cdf(-1*abs(Z))
-    pval[(n1 == 0) & (n2 == 0)] = 0
+    pval[(n1 == 0) & (n2 == 0)] = 1.
     
     return pval
 
